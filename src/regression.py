13f99bd68d2a0f01bb5dfc7471b15ff45c667a07
Vladislav Veselinov ACS2017 <iyb17178@telford-10.cis.strath.ac.uk>
2017-10-04 14:03:35 +0100

    Read data file

--- src/regression.py
+++ src/regression.py
@@ -1,11 +1,36 @@
-import numpy as np
-from numpy import trapz
+import os
+from re import sub
+from numpy import trapz, array
+from pprint import pprint
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))
+)
+
+txtFile = os.path.join(__location__, 'data-small.txt')
+
+data = {}
+currentKey = ''
+for line in open(txtFile, 'r'):
+    text = line.strip().replace(':', '')
+
+    if 'unit' in text:
+        currentKey = text
+        data[currentKey] = []
+
+    elif 'v' not in text:
+        data[currentKey].append(int(text))
 
 
-# The y values.  A numpy array is used here,
-# but a python list could also be used.
-y = np.array([5, 20, 4, 18, 19, 18, 7, 4])
+pprint(data)
+sort = sorted(data, key = lambda key: int(sub('[^0-9]','', key)))
+# pprint(sort)
 
-# Compute the area using the composite trapezoidal rule.
-area = trapz(y, dx=5)
-print("area =", area)
+
+# y = [5, 20, 4, 18, 19, 18, 7, 4]
+#
+# # Compute the area using the composite trapezoidal rule.
+# # https://en.wikipedia.org/wiki/Trapezoidal_rule
+#
+# for i in range(5):
+#     print(trapz(y))
