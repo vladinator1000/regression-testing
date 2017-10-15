48842a155e8e7e4228c4e77ed68d8e65f35c7f2c
Vlady Veselinov <vladkodmc@gmail.com>
2017-10-15 19:08:41 +0100

    Progress

--- src/regression.py
+++ src/regression.py
@@ -1,10 +1,9 @@
 import os
 import random
-from re import sub
 from pprint import pprint
-from matplotlib import pyplot
+# from matplotlib import pyplot
 
-#  Parse data
+# Parse .txt file
 __location__ = os.path.realpath(
 	os.path.join(os.getcwd(), os.path.dirname(__file__))
 )
@@ -25,13 +24,80 @@ for line in open(txtFile, 'r'):
 		data[currentKey].append(int(text))
 
 
-sampleKeys = random.sample(list(data), 5)
+# https://imgur.com/a/pLTxz 
+# Average Percentage of Faults Detected (Pandey and Shrivastava, 2011):
+def APFD(tests = [[]]):
+	numberOfTests = len(tests)
+	numberOfFaults = len(tests[0])
+	sumOfFaultIndexes = 0
 
-# sums = map(lambda key: sum(data[key]), sampleKeys)
-# pprint(sums)
+	# Initial list of zeros
+	faultsFound = [0 for i in range(numberOfFaults)]
+	
 
-for key in sampleKeys:
-	print()
+	for faults in tests:
+		for index, fault in enumerate(faults):
+			if fault == 1 and faultsFound[index] == 0:
+				# Add to index sum (counting from 1) 
+				sumOfFaultIndexes += index + 1
+				faultsFound[index] = 1
+			else:
+				sumOfFaultIndexes += numberOfFaults + 0.5
 
-# Each solution has 5 tests
-# Get APFD for each test (https://imgur.com/a/pLTxz)
+	return 1 - (sumOfFaultIndexes / (numberOfTests * numberOfFaults)) + (1 / (2 * numberOfTests))
+
+# This will make up our population 
+def randomTestsFromData(howMany = 5):
+	names = []
+	tests = []
+
+	for testName in random.sample(list(data), howMany):
+		names.append(testName)
+		tests.append(data[testName])
+
+	return (names, tests, APFD(tests))
+
+# Generate population :
+# List of tuples in the form of ((testName1, ...), (test1, ...), fitness)
+# firstGeneration = []
+
+# for i in range(1):
+# 	firstGeneration.append(randomTestsFromData())
+
+
+def tournament(generation = []):
+	first = random.choice(generation)
+	second = random.choice(generation)
+
+	# Fitness at index 3, bigger is better
+	if first[3] > second[3]:
+		return first
+	else:
+		return second
+
+def mutateGeneration(generation = [], probability = 0.15):
+	newGeneration = []
+
+	for chromosome in generation:
+		if random.random() < probability:
+			newChromosome = chromosome
+
+			# Replace a test inside a chromosome with a random one 
+			randomIndex = random.randrange(len(chromosome[1]))
+			newTestName = random.choice(list(data))
+
+			newChromosome[0][randomIndex] = newTestName
+			newChromosome[1][randomIndex] = data[newTestName]
+
+			newGeneration.append(newChromosome)
+			
+			print(newChromosome, )
+		else:
+			newGeneration.append(chromosome)
+
+# mutateGeneration(firstGeneration, 1)
+
+test = [[0, 0, 0, 0, 0, 0, 1, 0, 0] for i in range(5)]
+
+# print(test)
+print(APFD(test))
